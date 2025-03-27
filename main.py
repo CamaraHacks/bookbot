70af0fa8822102dc5dff6ccdebadf244b9da5a17
camarahacks <gustavo-camara@outlook.com>
2025-03-27 14:03:29 -0300

    refacto

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #file_path = /home/user0/workspace/github.com/bootdev/bookbot/frankenstein.txt
+from stats import count_words
 
-
-def get_book_text(file_path):
-    with open (file_path) as f:
+def get_book_text(filepath):
+    with open (filepath) as f:
         return f.read()
-        
+
 
 def main():
     path  = "books/frankenstein.txt"
@@ -12,16 +12,7 @@ def main():
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
     
-
-
-def count_words(text):
-    words = text.split()
-    return len(words)
         
-   
-
-    
-
 
 main()
 
